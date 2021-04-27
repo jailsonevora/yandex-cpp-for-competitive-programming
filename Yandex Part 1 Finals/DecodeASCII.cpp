d58e36e588181f1e30b63c8e1688e652b3e8320a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-04-27 15:34:50 -0100

    Decode Ascii

--- Yandex Part 1 Finals/DecodeASCII.cpp
+++ Yandex Part 1 Finals/DecodeASCII.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+// string recursive(string text, int i);
+// int recursive(int text);
+
+
+// string recursive(string text, int i){
+
+//         string plaintext;
+//         if(i == 0)
+//             return "";
+//         return plaintext +=char(recursive(text[i]));
+// }
+
+// int recursive(int text){
+//         return ( 100 * (recursive(text-2) - '0' ) )  + recursive(text-1);
+// }
+
+// int main(){
+    
+//     string text;  
+//     cin >> text;
+//     // for (int i = text.length(); i >= 0; i--)
+//     // {
+//         int i = text.length();
+//         cout << recursive(text, i) << "\n";
+//     // }
+// }
+
+
 int main(){
     string text, plainText;  
 
@@ -10,6 +38,7 @@ int main(){
 
     for (int i = 0, z = i, w = i; i < text.length(); i++, w++, z++)
     {   
+           
         int chTwoDigits = ( 10 * (text[i] - '0' ) ) + text[++i] - '0';
         w = i;
         int chThreeDigits = ( 100 * (text[z] - '0' ) ) + ( 10 * (text[w] - '0' ) ) + text[++w] - '0';
