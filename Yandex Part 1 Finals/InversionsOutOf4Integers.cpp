b246cef22068568366ce59be0f76c2df232861fe
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-04-29 20:14:00 -0100

    Inversion out of 4 integers

---
+++ Yandex Part 1 Finals/InversionsOutOf4Integers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <array>
+
+using namespace std;
+
+int main(){
+
+    int a, count = 0, swap = 0;
+    array<int, 4> input;
+
+    do{    
+        cin >> a;
+        input[count] = a;
+        count++;       
+    }while(count <= 3);
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        for (int j = i+1; j < input.size(); j++)
+        {
+            if(input[i] >=  input[j]){
+                int iTemp = input[i];
+                int jTemp = input[j];
+                input[j] = iTemp;
+                input[i] = jTemp;
+                swap++;
+            }
+        }        
+    }
+
+    cout << swap;
+}
