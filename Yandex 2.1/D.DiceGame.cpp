dfa5ebce954db9db3779b6dc7392c2c978af3f1b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-04-30 14:06:13 -0100

    Dice Game

--- Yandex 2.1/D.DiceGame.cpp
+++ Yandex 2.1/D.DiceGame.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
 
-    int a,b,c;
-    cin >> a >> b >> c;
+    int a1,a2,b1,b2;
+    cin >> a1 >> a2 >> b1 >> b2;
+    cout <<  (  ((a1 >= b1) || (a1 >= b2)) ||  ((a2 >= b1) || (a2 >= b2))  ) << "\n";
 }
