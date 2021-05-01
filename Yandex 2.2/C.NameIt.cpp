2efc4fbcb1548447e0004580b2deedc0623a75e1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-01 21:55:38 -0100

    Name it

---
+++ Yandex 2.2/C.NameIt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a;
+    cin >> a;
+    if(a == 1) cout << "one";
+    if(a == 2) cout << "two";
+    if(a == 3) cout << "three";
+    if(a == 4) cout << "four";
+    if(a == 5) cout << "five";
+    if(a == 6) cout << "six";
+    if(a == 7) cout << "seven";
+    if(a == 8) cout << "eight";
+    if(a == 9) cout << "nine";
+    if(a == 10) cout << "ten";
+}
