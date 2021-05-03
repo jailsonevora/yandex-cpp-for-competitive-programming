bf79917ed9016b372ad5c3dff3462b2e2ef3b76e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-03 16:06:28 -0100

    B. Count The Integers

---
+++ Yandex 2.3/B.CountTheIntegers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, count = 0;
+
+    while(cin >> a)
+    	count++;
+
+    cout << count;
+}
+
+
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     int a, count = 0;
+
+//     do{     
+//         count++;
+//     }while(cin >> a);
+
+//     cout << count-1;
+// }
+
