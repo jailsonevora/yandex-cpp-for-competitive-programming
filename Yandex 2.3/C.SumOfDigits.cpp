e95212d2e27bf907ebfe81cdf892ba26596d31df
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-04 00:52:48 -0100

    C. Sum Of Digits

---
+++ Yandex 2.3/C.SumOfDigits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, count = 0;
+
+    while(cin >> a)
+    	count++;
+
+    cout << count;
+}
