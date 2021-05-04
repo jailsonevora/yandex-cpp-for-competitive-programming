020be6a02f64aa89544640c26028193bc859b3cb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-04 15:27:35 -0100

    D. Next Lucky Number

---
+++ Yandex 2.3/D.NextLuckyNumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int sumDigits(int a);
+
+int sumDigits(int a){
+    int sum = 0;
+
+    int total_digts = ((int)log10(a) + 1);
+
+    for(; total_digts > 0; total_digts--){
+        int lastDigits = a % 10;   
+        sum += lastDigits;
+        a /= 10; 
+    }
+    return sum;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, sum = 0;
+    cin >> a;
+
+    a = abs(a);
+    sum = sumDigits(a);
+
+    while(sum != 13)       
+        sum = sumDigits(a+1);
+
+    cout << sum;
+}
