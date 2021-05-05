1b957d24677bee35deb82347f13738881dda4bfd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-05 22:20:51 -0100

    Interresting Divisor

--- Yandex Part 2 Finals/E.InterestingDivisor.cpp
+++ Yandex Part 2 Finals/E.InterestingDivisor.cpp
@@ -8,12 +8,12 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    long long l, r, count = 0;
+    long long l, r, count = 0, n;
 
     cin >> l >> r;
 
-    for (long long n = l; (n % (n+1) == 0) && n <= r; ++n)
+    for (n = l; (n % (n+1) == 0) && n <= r; n++)
         count++;
 
-    cout << count;   
+    cout << fixed << setprecision(0) << count;   
 }
