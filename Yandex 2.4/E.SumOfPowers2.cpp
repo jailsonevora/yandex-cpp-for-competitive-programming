5f18b7130cccade99e11af9f246893d894d0d240
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-05 16:07:28 -0100

    Finde the counter exemple

--- Yandex 2.4/E.SumOfPowers2.cpp
+++ Yandex 2.4/E.SumOfPowers2.cpp
@@ -4,15 +4,24 @@
 
 using namespace std;
 
+void prime(int n);
+
+void prime(int n){
+    if (n == 2 || (n % 2 == 1 && n != 9 && n != 1)) {  
+        cout << "Prime" << endl;  
+    } else {  
+        cout << "Not Prime" << endl;  
+    } 
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    long double a, n;  
-    long double p = 1;  
-    cin >> a >> n;
+    // int n;  
+    // cin >> n;
 
-    for (int i = 1; i <= n; i++)
-      (i % 2 == 0) ? (p = p + pow(a,i)) : (p = p - pow(a,i));
-    cout << fixed << std::setprecision(0) << (p);     
+    for (int i = 0; i < 100; ++i){
+        cout << i << ": "; prime(i);      
+    }
 }
