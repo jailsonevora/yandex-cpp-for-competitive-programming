fa332289277a160df1fe15ad23150e30eff602cf
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-05 00:35:38 -0100

    Above the limits

--- Yandex 2.3/A.AboveTheLimits.cpp
+++ Yandex 2.3/A.AboveTheLimits.cpp
@@ -3,20 +3,25 @@
 
 using namespace std;
 
+int totalDigits(int a);
+
+int totalDigits(int a){
+    return int(log10(a) + 1);  
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int a, c = 1;
+    int a, c = 0;
     cin >> a;
 
-    for(; ; c++){
-        a +=a;        
-        int total_digts = ((int)log10(a) + 1);
-        if(total_digts >= 8)
-            break;
-    }
-    cout << a << " " << c;
+    do{
+        a++;
+        c++;
+    }while(totalDigits(a) < 8);
+
+    cout << a << c;
 }
 
 // #include <iostream>
