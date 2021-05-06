fac5f9d94a68fa0fe983af6b391ea3674e71fbdf
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-06 00:51:21 -0100

    Interesting divisor bug fixed

--- Yandex Part 2 Finals/E.InterestingDivisor.cpp
+++ Yandex Part 2 Finals/E.InterestingDivisor.cpp
@@ -12,8 +12,29 @@ int main(){
 
     cin >> l >> r;
 
-    for (n = l; (n % (n+1) == 0) && n <= r; n++)
+    for (n = l; ( (n+1) % n == 0) && n <= r ; n++)
         count++;
 
-    cout << fixed << setprecision(0) << count;   
+    cout << fixed << count;   
 }
+
+// or
+// #include <iostream>
+// #include <cmath>
+// #include <iomanip>
+
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+// 	long long l, r;
+
+// 	cin >> l >> r;
+
+//     long long count = 0;
+// 	for (long long i = l, j = l+1; (j%i == 0) && i <= r; i++, j++)
+// 		 count++;
+//     cout << fixed << count;   
+// }
