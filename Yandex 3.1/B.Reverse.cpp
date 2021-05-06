cf041c34776923a84b0ef48464c58dbb86aee93e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-06 16:19:30 -0100

    B. Reverse

---
+++ Yandex 3.1/B.Reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    int a[n];
+
+    cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for(int i = n-1; i > 0 ; --i)
+        cout << a[i] << " ";
+}
