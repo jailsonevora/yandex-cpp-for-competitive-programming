de0ec565430c3a6402eed4999eb5997e67aec095
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-08 01:30:00 -0100

    E. Kinght Targets

--- Yandex 3.2/E.KinghtTargets.cpp
+++ Yandex 3.2/E.KinghtTargets.cpp
@@ -7,12 +7,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n = 0, m = 0;
-    cin >> n;
-
-    m = n;
-    int a[n][m];
-    cin.clear();
+    int n = 8, m = 8;
 
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < m; ++j)
