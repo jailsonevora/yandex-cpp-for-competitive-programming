1eef6b7d1743b7c3bdc868b9042876b410f8ad0f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-09 20:32:15 -0100

    E. Kinght Targets

--- Yandex 3.2/E.KinghtTargets.cpp
+++ Yandex 3.2/E.KinghtTargets.cpp
@@ -5,13 +5,11 @@ using namespace std;
 
 #define n 6
 #define m 6
-
 bool isSafe(int x, int y, char a[n][m]);
 
 bool isSafe(int x, int y, char a[n][m])
 {
-    return (x >= 0 && x < n && y >= 0 && y < m
-            && a[x][y] == '.');
+    return (x >= 0 && x < n && y >= 0 && y < m && a[x][y] == '.');
 }
 
 int main(){
@@ -25,42 +23,48 @@ int main(){
             cin >> a[i][j];
 
     int ki = -1, kj = -1;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             if(a[i][j] == 'k'){                
                 ki = i;
                 kj = j;               
             }
-        }  
-    }
 
     // move up to the left
-    if(isSafe(ki-2, kj-1, a))
+    if(isSafe(ki-2, kj-1, a)){
         a[ki-2][kj-1] = '!'; a[ki-2][kj-1] = '!';
+    }
     // move up to the right
-    if(isSafe(ki-2, kj+1, a))
+    if(isSafe(ki-2, kj+1, a)){
         a[ki-2][kj+1] = '!'; a[ki-2][kj+1] = '!';
+    }
 
     // move down to the left
-    if(isSafe(ki+2, kj-1, a))
+    if(isSafe(ki+2, kj-1, a)){
         a[ki+2][kj-1] = '!'; a[ki+2][kj-1] = '!';
+    }
     // move down to the right
-    if(isSafe(ki+2, kj+1, a))
-        a[ki+2][kj+1] = '!'; a[ki+2][kj+1] = '!'; 
+    if(isSafe(ki+2, kj+1, a)){
+        a[ki+2][kj+1] = '!'; a[ki+2][kj+1] = '!';
+    }
 
     // move horizontal to the left up 
-    if(isSafe(ki-1, kj-2, a))
+    if(isSafe(ki-1, kj-2, a)){
         a[ki-1][kj-2] = '!'; a[ki-1][kj-2] = '!';
+    }
     // move horizontal to the left down
-    if(isSafe(ki+1, kj-2, a))
+    if(isSafe(ki+1, kj-2, a)){
         a[ki+1][kj-2] = '!'; a[ki+1][kj-2] = '!';
+    }
 
     // move horizontal to the right up 
-    if(isSafe(ki-1, kj+2, a))
+    if(isSafe(ki-1, kj+2, a)){
         a[ki-1][kj+2] = '!'; a[ki-1][kj+2] = '!';
+    }
     // move horizontal to the right down
-    if(isSafe(ki+1, kj+2, a))
-        a[ki+1][kj+2] = '!'; a[ki+1][kj+2] = '!';    
+    if(isSafe(ki+1, kj+2, a)){
+        a[ki+1][kj+2] = '!'; a[ki+1][kj+2] = '!'; 
+    }   
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++)
