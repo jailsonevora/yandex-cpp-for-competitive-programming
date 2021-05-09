d73ea44093fdce3ed17006e71dd82c12f9f6333f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-09 20:58:46 -0100

    E. Kinght Targets

--- Yandex 3.2/E.KinghtTargets.cpp
+++ Yandex 3.2/E.KinghtTargets.cpp
@@ -70,7 +70,7 @@ int main(){
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++)
-            cout << a[i][j] << " ";
+            cout << a[i][j] << "";
         cout<<endl;
     }
 }
