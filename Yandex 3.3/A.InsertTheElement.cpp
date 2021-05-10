fdb7e221ca68c87bcfcdc541a7e454c95a74f208
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-10 13:09:06 -0100

    A. Insert The Element

--- Yandex 3.3/A.InsertTheElement.cpp
+++ Yandex 3.3/A.InsertTheElement.cpp
@@ -7,20 +7,25 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n = 0, m = 0;
-    cin >> n >> m;
+    int n = 5;
 
-    int a[n][m];
+    int a[n];
     cin.clear();
 
     for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            cin >> a[i][j];
+            cin >> a[i];
 
-    for (int j = 0; j < m; ++j){
-        long long sum = 0;
-        for (int i = 0; i < n; ++i) 
-            sum +=a[i][j];
-        cout << sum << " ";
+    int aTemp[n];
+    int t = 3; 
+    for(int i = 0; i < n; ++i){
+        if(t >= a[i]){
+            aTemp[i] = t;
+            t = a[i];
+        }    
+        else
+            aTemp[i] = a[i];
     }
+
+    for (int i = 0; i < n; ++i)
+        cout << aTemp[i] << " ";
 }
