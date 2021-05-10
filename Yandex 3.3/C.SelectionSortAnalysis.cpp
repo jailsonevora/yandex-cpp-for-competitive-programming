4636a1b9ff9517c3881410096d7a3bc6b11aa85c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-10 16:13:50 -0100

    C. Selection Sort Analysis

--- Yandex 3.3/C.SelectionSortAnalysis.cpp
+++ Yandex 3.3/C.SelectionSortAnalysis.cpp
@@ -3,17 +3,18 @@
 
 using namespace std;
 
-void swap(int *i, int *j){
+int swap(int *i, int *j){
     int temp = *j;
     *j = *i;
     *i = temp;
+    return 1;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n = 0;
+    int n = 0, numOfSwap;
     cin >> n;
 
     int a[n];
@@ -22,14 +23,15 @@ int main(){
     for (int i = 0; i < n; ++i)
             cin >> a[i];
 
-    for(int i = 0, midI = i; i < n; ++i){
-        for(int j = 0; j < n; ++j)
+    for(int i = 0; i < n; ++i){
+        int midI = i;
+        for(int j = i+1; j < n; ++j)
             if(a[j] < a[midI])
                 midI = j;
-        swap(&a[midI], &a[i]);
+        numOfSwap = swap(&a[midI], &a[i]);
     }
     
     for (int i = 0; i < n; ++i)
-        cout << a[i] << " ";
+        cout << numOfSwap << " ";
 }   
 
