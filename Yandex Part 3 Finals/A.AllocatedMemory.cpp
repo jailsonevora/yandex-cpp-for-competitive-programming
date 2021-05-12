3d866f95c0c0e582a25d084424d3074ae0751dc3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 12:06:19 -0100

    A. Allocated Memory

---
+++ Yandex Part 3 Finals/A.AllocatedMemory.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int a[30];  
+    long long b[239];  
+    char ch;  
+    unsigned long long q;  
+    char txt[15];
+
+    cout << "Total: " << sizeof(a) + sizeof(b) + sizeof(ch) + sizeof(q) + sizeof(txt) << "\n";
+    cout << "a: " << sizeof(a) << " ";
+    cout << "b: " << sizeof(b) << " ";
+    cout << "ch: " << sizeof(ch)<< " ";
+    cout << "q: " << sizeof(q) << " ";
+    cout << "txt: " << sizeof(txt) << " ";    
+}   
