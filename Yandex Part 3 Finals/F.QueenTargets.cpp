358c0e3eb0fb9e792a3f4f0888f672d3396dfb29
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 14:55:45 -0100

    F. Queen Targets

---
+++ Yandex Part 3 Finals/F.QueenTargets.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define n 4
+#define m 4
+bool isSafe(int x, int y, char a[n][m]);
+
+bool isSafe(int x, int y, char a[n][m])
+{
+    return (x >= 0 && x < n && y >= 0 && y < m && int(a[x][y]) == 46/*'.'*/);
+}
+
+void movingOnVertical(int kj, char a[n][m])
+{
+    for (int i = 0; i < n; i++)
+        if(isSafe(i, kj, a))
+            a[i][kj] = '!';
+}
+
+void movingOnHorizontal(int ki, char a[n][m])
+{
+    for (int j = 0; j < m; j++)
+        if(isSafe(ki, j, a))
+            a[ki][j] = '!';
+}
+
+void movingOnDiagonal(int ki, int kj, char a[n][m])
+{
+    for (int i = 0, j = 0; i < n && j < n; i++, ++j)
+            if(isSafe(i, j, a)) a[i][j] = '!';
+    for (int i = 0, j = n-1; i < n && j >= 0; ++i, --j)
+            if(isSafe(i, j, a)) a[i][j] = '!';
+}
+
+void printingMatrix(char a[n][m])
+{
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            cout << a[i][j] << "";
+        cout<<endl;
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    char a[n][m];
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> a[i][j];
+
+    int ki = 9, kj = 9;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if(a[i][j] == 'F'){                
+                ki = i;
+                kj = j;               
+            }
+        }
+    }
+
+    // move vertical
+    movingOnVertical(kj, a);
+    // move horizontal
+    movingOnHorizontal(ki,a);   
+    // move diagonals
+    movingOnDiagonal(ki, kj, a);  
+
+    printingMatrix(a);
+    
+}
