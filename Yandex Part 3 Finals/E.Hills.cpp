0424c1a82b81f8e6aa759e58b2cfe82785c6d21d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 13:41:34 -0100

    E. Hills

---
+++ Yandex Part 3 Finals/E.Hills.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int hills(int n, int a[]){
+    int count = 0;
+    for(int i = 1; i < n - 2; ++i)  
+        if(a[i] > a[i-1] && a[i] > a[i+1])
+            count++;
+    return count;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n = 0;
+    cin >> n;  
+
+    int a[n];
+    cin.clear();
+    for (int i = 0; i < n; ++i)
+            cin >> a[i];  
+    
+    cout << hills(n, a);    
+}   
