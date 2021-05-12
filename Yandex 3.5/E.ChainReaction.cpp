fa96a35fab1a45d08f13e815b8342a97b2487b42
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 11:54:59 -0100

    B. Correct Data Type Processing

--- Yandex 3.5/E.ChainReaction.cpp
+++ Yandex 3.5/E.ChainReaction.cpp
@@ -9,24 +9,6 @@ int f(int n, int x){
     x++;
     return f(n - 1, x) + f(n - 2, x); 
 }
-
-// void f(int a, int b) {  
-//     return a + b;  
-// }
-// int f(double a, double b) {  
-//     return a + b;  
-// }
-// int f(int a, int b) {  
-//     return a + b;  
-// }
-// long long f(int a, int b) {  
-//     return a + b;  
-// }
-// void f(int a, int b) {  
-//     int c = a + b;  
-//     return;  
-// }
-
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
