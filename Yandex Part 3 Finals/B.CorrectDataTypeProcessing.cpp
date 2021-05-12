5ec9ba9569af280e64efa1f4cc91e60098b534ca
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 11:57:45 -0100

    B. Correct Data Type Processing

--- Yandex Part 3 Finals/B.CorrectDataTypeProcessing.cpp
+++ Yandex Part 3 Finals/B.CorrectDataTypeProcessing.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 //Which functions does not cause compilation errors nor implicit type conversion?
-void f(int a, int b) {  
-    return a + b;  
-}
+// void f(int a, int b) {  
+//     return a + b;  
+// }
 int f(double a, double b) {  
     return a + b;  
 }
-int f(int a, int b) {  
-    return a + b;  
-}
-long long f(int a, int b) {  
-    return a + b;  
-}
-void f(int a, int b) {  
-    int c = a + b;  
-    return;  
-}
+// int f(int a, int b) {  
+//     return a + b;  
+// }
+// long long f(int a, int b) {  
+//     return a + b;  
+// }
+// void f(int a, int b) {  
+//     int c = a + b;  
+//     return;  
+// }
 
 int main(){
     ios::sync_with_stdio(false);
