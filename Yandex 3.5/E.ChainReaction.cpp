90cfa67d349ef7d75fb38afccd32cf79ae03be2a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-12 11:53:40 -0100

    A. Allocated Memory

--- Yandex 3.5/E.ChainReaction.cpp
+++ Yandex 3.5/E.ChainReaction.cpp
@@ -3,19 +3,39 @@
 
 using namespace std;
 
-void f(int n){
-    if(n <= 0)
-        return;
-    cout << n << " ";
-    f(n - 1);
+int f(int n, int x){
+    if(n <= 1)
+        return n;
+    x++;
+    return f(n - 1, x) + f(n - 2, x); 
 }
+
+// void f(int a, int b) {  
+//     return a + b;  
+// }
+// int f(double a, double b) {  
+//     return a + b;  
+// }
+// int f(int a, int b) {  
+//     return a + b;  
+// }
+// long long f(int a, int b) {  
+//     return a + b;  
+// }
+// void f(int a, int b) {  
+//     int c = a + b;  
+//     return;  
+// }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n = 0;
-    cin >> n;
+    // int n = 0;
+    // cin >> n;
 
-    f(n);    
+    //f(2,5);
+    cout << f(2,5);
+
+    //cout << f(n, 0);    
 }   
