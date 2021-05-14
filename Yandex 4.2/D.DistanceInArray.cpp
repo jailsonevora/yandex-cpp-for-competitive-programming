46a349a23b50c669fde34d437bf57e99c90e6be0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-14 18:03:59 -0100

    D. Distance In Array

--- Yandex 4.2/D.DistanceInArray.cpp
+++ Yandex 4.2/D.DistanceInArray.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int dist(int &a, int &b){
+int dist(int *a, int *b){
 
     return ((b - a) + 1);
 }
@@ -12,8 +12,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int x[] = {2,3,6,37,5,8,6,4,6};   
-
-    dist(x[3], x[8]);
-    cout << "Distance : "<< dist(x[3], x[8]) <<"\n";
+    int x[] = {2,3,6,37,5,8,6,4,6};
+    
+    cout << "Distance : "<< dist(&x[3], &x[8]) <<"\n";
 }   
