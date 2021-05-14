8316d60022d6c8eb89b7d7a9d790fa8570ce8f29
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-14 17:22:53 -0100

    B. Check for NULL

---
+++ Yandex 4.2/B.CheckforNULL.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//by reference with pointer
+bool check(int *a){
+    return (a == NULL) ? true : false; 
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n = 4;
+
+    int *a = &n;
+    cout << "a before: " << *a << "\n";    
+    
+    //call by reference
+    //check(a);
+    //call by reference with pointer
+
+    cout << "a after: " << check(a) << "\n";
+}   
