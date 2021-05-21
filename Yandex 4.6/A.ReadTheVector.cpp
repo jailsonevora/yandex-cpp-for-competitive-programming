610a9b692990e836eafe9d3b06040dab141689e5
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-21 13:25:40 -0100

    A. Read The Vector

--- Yandex 4.6/A.ReadTheVector.cpp
+++ Yandex 4.6/A.ReadTheVector.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <iterator>
+#include <sstream>
 
 using namespace std;
 
-void read(std::vector<int> &v)
+void read()
+{
+    std::string line;
+    std::vector<std::vector<int>> tmp;
+    while (std::getline(std::cin, line)) {
+        std::istringstream bufferInput(line);
+        tmp.emplace_back(std::istream_iterator<int>{bufferInput}, std::istream_iterator<int>{});
+    }
+}
+
+void read(std::vector<int> &v, int n)
 {
     int a;
-    cin.clear();
     while (cin >> a){
-        v.push_back(a);
         cin.clear();
+        v.push_back(a);        
     }
 };
+
+void read(std::vector<int> &v){
+    std::string line;
+    cin.ignore();
+    getline(cin, line);
+
+    for (int i = 0; i < line.size(); i++)
+        v.push_back(int(line[i]));
+}
 
 int main(){
     ios::sync_with_stdio(false);
