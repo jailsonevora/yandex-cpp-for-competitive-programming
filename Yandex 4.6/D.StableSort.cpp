2657bb34b9f3bb4c787756b822627aff79091f4f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-24 16:49:27 -0100

    D. Stable Sort

--- Yandex 4.6/D.StableSort.cpp
+++ Yandex 4.6/D.StableSort.cpp
@@ -4,18 +4,19 @@
 #include <iterator>
 #include <sstream>
 #include <fstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 void sort_vector(vector <vector <int> > &vec){
-    
+    stable_sort(vec.begin(), vec.begin() + vec.size());
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    vector<vector<int>> v;
+    vector<vector<int>> matrix;
     int n = 0, count = 0;
     int arrParam[3];
     std::string line; int num;
@@ -34,10 +35,20 @@ int main(){
             stringstream ss(line);
             for (int i = 0; !ss.eof(); i++){
                 ss >> num;
-                arrParam[i] = num;
+                std::vector<std::vector<int>> matrix(n, std::vector<int>(n, num));
             }
         }
-    }   
+    } 
+
+    sort_vector(matrix); 
+
+    for (std::vector<int> row: matrix)
+    {
+        for (int val: row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
     
     MyReadFile.close();
 }   
