e7f1540d99ab121184ac2b3e27db835009e31996
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-24 11:25:44 -0100

    C. The Member Functions of Vector

---
+++ Yandex 4.6/C.TheMemberFunctionsofVector.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    std::string line;
+    vector<int> v = {1,4,5,7,23,52,9};
+    int n = 0, t = 0, size = 0, val = 0, ith = 0;
+
+    cin >> n;
+    cin.ignore();
+    getline(cin, line);
+
+    t = line[0];
+
+        switch (t)
+        {
+            case 1:{
+                size = line[1]; 
+                v.resize(size);
+            }break;
+            
+
+            case 2:
+                n = line[1];
+                val = line[2];
+                v.assign(n,val);
+            break;
+
+            case 3:
+                v.clear();
+            break;
+
+            case 4:
+                ith = line[1];
+                cout << v[ith] << "\n";
+            break;
+
+            case 5:
+            /* code */
+            break;
+
+            case 6:
+            /* code */
+            break;
+
+            case 7:
+            /* code */
+            break;
+
+            case 8:
+            /* code */
+            break;
+
+            case 9:
+            /* code */
+            break;
+
+            case 10:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+
+    
+
+}   
