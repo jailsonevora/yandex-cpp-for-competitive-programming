829e9d9b6bc7aca1f5af6bfef10a2640dac03eb9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-25 10:02:03 -0100

    D. Stable Sort

--- Yandex 4.6/D.StableSort.cpp
+++ Yandex 4.6/D.StableSort.cpp
@@ -12,6 +12,12 @@ void sort_vector(vector <vector <int> > &vec){
     stable_sort(vec.begin(), vec.begin() + vec.size());
 }
 
+void sort_vector(vector <vector <int> > &vec){
+    
+}
+
+
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -24,31 +30,38 @@ int main(){
 
     ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 4.6/inputD.txt");
 
-        
-    while(getline(MyReadFile, line)){
-        if(count == 0){
-            stringstream ss(line);
-                ss >> n;
-            count++;
-        }
-        else{
-            stringstream ss(line);
-            for (int i = 0; !ss.eof(); i++){
-                ss >> num;
-                std::vector<std::vector<int>> matrix(n, std::vector<int>(n, num));
+    
+        // construct a vector of ints with the given default value
+        std::vector<int> v;    
+        while(getline(MyReadFile, line)){
+            if(count == 0){
+                stringstream ss(line);
+                    ss >> n;
+                count++;
+            }
+            else{
+                    for (int i = 0; i < n; i++)
+                    {
+                        stringstream ss(line);
+                        for (int i = 0; !ss.eof(); i++){
+                            ss >> num;
+                            v.push_back(num);
+                        }
+                        matrix.push_back(v);
+                    }
             }
         }
-    } 
+    }
 
     sort_vector(matrix); 
 
-    for (std::vector<int> row: matrix)
-    {
-        for (int val: row) {
-            std::cout << val << " ";
-        }
-        std::cout << std::endl;
-    }
+    // for (std::vector<int> row: matrix)
+    // {
+    //     for (int val: row) {
+    //         std::cout << val << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
     
     MyReadFile.close();
 }   
