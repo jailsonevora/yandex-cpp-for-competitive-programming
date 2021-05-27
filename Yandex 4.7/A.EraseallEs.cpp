30819c57c25fa67beb4f0d86820e6d9fef7aaadd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-27 10:31:14 -0100

    A. Erase all e's

--- Yandex 4.7/A.EraseallEs.cpp
+++ Yandex 4.7/A.EraseallEs.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
 
-void e_rase(){
-    
+using namespace std;
+
+void e_rase(string &e){
+    for(int i = 0, j = 1; i < e.length(); ++i, ++j){
+        if(e.substr(i,j) == "e")
+            e.erase('e');
+    }
 }
 
 int main(){
 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string e = "afegsaegddfhafe";
+    e_rase(e);
 }
