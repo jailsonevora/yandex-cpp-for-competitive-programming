cb946224e83a02a28965cff92285cabb62753567
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-27 15:41:23 -0100

    D. Count the Words and the Lines

--- Yandex 4.7/D.CounttheWordsandtheLines.cpp
+++ Yandex 4.7/D.CounttheWordsandtheLines.cpp
@@ -6,24 +6,36 @@
 
 using namespace std;
 
-int main(){
 
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+int countLines(){
 
-    string line; 
-    
-    int countWords=0;
+    string line;
     int countNoEmptyLine = 0;
+
+    ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 4.7/inputD.txt");
+
+    while(!getline(MyReadFile, line).eof()){
+        stringstream ss(line); 
+
+        if(!line.empty())
+            countNoEmptyLine++;                   
+    }
+
+    MyReadFile.close();
+
+    return countNoEmptyLine;
+}
+
+int countWords(){
+
+    string line;
+    int countWords=0;
 
     ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 4.7/inputD.txt");
 
     while(getline(MyReadFile, line)){
         stringstream ss(line); 
 
-        if(!line.empty())
-            countNoEmptyLine++;
-        
         while (!ss.eof()){
             string words;
             ss >> words;
@@ -31,8 +43,17 @@ int main(){
                 countWords++;            
         }                    
     }
-
-    cout << countNoEmptyLine<< " " << countWords;
-
     MyReadFile.close();
+
+    return countWords;
+}
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << countLines() << " " << countWords();
+    
 }
