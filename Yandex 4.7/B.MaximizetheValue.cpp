21a8b4f298528969eb18b93aa30645c4c9396607
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-27 12:04:51 -0100

    C. Animal Farm

---
+++ Yandex 4.7/B.MaximizetheValue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void maximal(long long &){
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long n = 9;
+    maximal(n);
+    cout << n;
+}
---
+++ Yandex 4.7/C.AnimalFarm.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+bool iequals(const string& a, const string& b)
+{
+    return std::equal(a.begin(), a.end(),
+                      b.begin(), b.end(),
+                      [](char a, char b) {
+                          return tolower(a) == tolower(b);
+                      });
+}
+
+void e_rase(string &e){
+
+    if(iequals(e,"cat"))
+        cout << "meow" << "\n";
+    else if(iequals(e,"dog"))
+        cout << "woof" << "\n";
+    else if(iequals(e,"pig"))
+        cout << "grunt" << "\n";
+    else if(iequals(e,"rooster"))
+        cout << "cock-a-doodle-doo" << "\n";
+    else if(iequals(e,"cow"))
+        cout << "moo" << "\n";
+    else 
+        cout << "all-animals-are-equal" << "\n";             
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string line;
+
+    ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 4.7/inputC1.txt");
+
+    while(getline(MyReadFile, line)){
+        e_rase(line);
+    }
+
+    MyReadFile.close();
+}
