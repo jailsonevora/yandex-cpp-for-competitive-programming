e1613e6844d82369f0bb79a7a1c56ade3af859fd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-28 13:15:53 -0100

    A. Fast Read

--- Yandex 5.1/A.FastRead.cpp
+++ Yandex 5.1/A.FastRead.cpp
@@ -1,35 +1,55 @@
+// #include <iostream>
+// #include <string>
+// #include <sstream>
+// #include <fstream>
+
+// using namespace std;
+
+// int main(){
+
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+
+//     string line;
+
+//     ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 5.1/inputA.txt");
+//     int sum = 0, i = 0;
+
+//     while(getline(MyReadFile, line)){
+//         stringstream ss(line);
+
+//         while (!ss.eof()){
+//             int temp;
+//             ss >> temp;
+//             if(i > 0)
+//                 sum+=temp;
+//             else;
+//             i++;                     
+//         }
+//     }    
+//     cout << sum;
+
+//     MyReadFile.close();    
+// }
+
+// OR
+
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
 
 using namespace std;
 
 int main(){
 
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    long long line;
+    long long sum = 0; int i = 0;
 
-    string line;
-
-    ifstream MyReadFile("C:/Competitive Programming/yandex-cpp-for-competitive-programming/Yandex 5.1/inputA.txt");
-    int sum = 0, i = 0;
-
-    while(getline(MyReadFile, line)){
-        stringstream ss(line);
-
-        while (!ss.eof()){
-            int temp;
-            ss >> temp;
+    while(scanf("%lld", &line)==1){
             if(i > 0)
-                sum+=temp;
+                sum+=line;
             else;
-            i++;                     
-        }
+            i++;
     }    
-    cout << sum;
-
-    MyReadFile.close();
+    printf("%lld",sum);
     
 }
