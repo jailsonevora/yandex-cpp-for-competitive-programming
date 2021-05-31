d3b4a182b733404e8520d04278646bfbd0ed4a4a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-31 13:15:02 -0100

    B. Binary to Decimal

--- Yandex 5.2/B.BinarytoDecimal.cpp
+++ Yandex 5.2/B.BinarytoDecimal.cpp
@@ -19,22 +19,24 @@ long long binaryToDecimal(long long n)
     
     return decimal;
 }
-
-string decimalToBinaryS(long long n)
-{
-    //finding the binary form of the number and
-    //coneverting it to string.
-    string s = bitset<64> (n).to_string();
-     
-    //Finding the first occurance of "1"
-    //to strip off the leading zeroes.
-    const auto loc1 = s.find('1');
-     
-    if(loc1 != string::npos)
-        return s.substr(loc1);
-     
-    return "0";
+long long binaryToDecimal(long long n)
+{   
+    string num = bitset<64> (n).to_string();
+    long long dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    long long base = 1;
+ 
+    long long len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+ 
+    return dec_value;
 }
+ 
 
 int main(){
 
