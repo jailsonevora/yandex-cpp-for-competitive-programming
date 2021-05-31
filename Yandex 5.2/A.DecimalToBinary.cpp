6b863833e0bef39e35929d2de8544b15d8ffc7e9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-31 12:05:12 -0100

    A. Decimal To Binary

--- Yandex 5.2/A.DecimalToBinary.cpp
+++ Yandex 5.2/A.DecimalToBinary.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-// void decToBinary(long long n)
-// {
-//     long long binaryNum[32];
-//     for (int j = 0; j < 32; j++)
-//         binaryNum[j] = -1;
+void decToBinary(long long n)
+{
+    long long binaryNum[32];
+    for (int j = 0; j < 32; j++)
+        binaryNum[j] = -1;
  
-//     int i = 0;
-//     while (n > 0) {
+    int i = 0;
+    while (n > 0) {
  
-//         binaryNum[i] = n % 2;
-//         n = n / 2;
-//         i++;
-//     }
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
 
-//     for (int j = i - 1; j >= 0; j--)
-//         cout << binaryNum[j];
-// }
+    for (int j = i - 1; j >= 0; j--)
+        cout << binaryNum[j];
+}
 
 int decimalToBinary(long long N)
 {
