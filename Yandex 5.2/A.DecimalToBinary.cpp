6e41ad2370531204ef9643f640ffdddac8b76442
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-05-31 12:04:56 -0100

    A. Decimal To Binary

--- Yandex 5.2/A.DecimalToBinary.cpp
+++ Yandex 5.2/A.DecimalToBinary.cpp
@@ -21,21 +21,17 @@ using namespace std;
 //         cout << binaryNum[j];
 // }
 
-#define ull unsigned long long int
-
-void decimalToBinary(int N)
+int decimalToBinary(long long N)
 {
  
     // To store the binary number
-    ull B_Number = 0;
+    unsigned long long B_Number = 0;
     int cnt = 0;
     while (N != 0) {
         int rem = N % 2;
-        ull c = pow(10, cnt);
+        unsigned long long c = pow(10, cnt);
         B_Number += rem * c;
         N /= 2;
- 
-        // Count used to store exponent value
         cnt++;
     }
  
@@ -46,5 +42,5 @@ int main(){
 
     long long n;
     cin >> n;
-    decToBinary(n);
+    cout << decimalToBinary(n);
 }
