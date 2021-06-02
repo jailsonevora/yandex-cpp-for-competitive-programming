7968d7a5f6e60f4c63d9b67ac351a22bea019d94
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-02 10:17:19 -0100

    D. Base K to Decimal

--- Yandex 5.2/D.BaseKtoDecimal.cpp
+++ Yandex 5.2/D.BaseKtoDecimal.cpp
@@ -10,20 +10,20 @@ using namespace std;
 int val(char c)
 {
     if (c >= '0' && c <= '9')
-        return (int)c - '0';
+        return (long long)c - '0';
     else
-        return (int)c - 'A' + 10;
+        return (long long)c - 'A' + 10;
 }
  
 // Function to convert a number from given base 'b'
 // to decimal
-void baseToDeci(string str, int base)
+void baseToDeci(string str, long long base)
 {
     
-    int len = str.size();
-    int power = 1; // Initialize power of base
-    int num = 0;  // Initialize result
-    int i;
+    long long len = str.size();
+    long long power = 1; // Initialize power of base
+    long long num = 0;  // Initialize result
+    long long i;
  
     // Decimal equivalent is str[len-1]*1 +
     // str[len-2]*base + str[len-3]*(base^2) + ...
@@ -31,8 +31,10 @@ void baseToDeci(string str, int base)
     {
         // A digit in input number must be
         // less than number's base
-        if (val(str[i]) >= base)
-            printf("Invalid Number");
+        if (val(str[i]) >= base){
+            printf("Invalid Number\n");
+            break;
+        }
  
         num += val(str[i]) * power;
         power = power * base;
@@ -48,7 +50,7 @@ int main(){
 
     cin.clear();
 
-    int k;
+    long long k;
     cin >> k;
 
     baseToDeci(n, k);
