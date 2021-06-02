9fc9608930fe6ebdbc9bb0fb637a26c7d9e12614
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-02 10:57:58 -0100

    E. Base K to Base T

--- Yandex 5.2/E.BaseKtoBaseT.cpp
+++ Yandex 5.2/E.BaseKtoBaseT.cpp
@@ -7,6 +7,17 @@ using namespace std;
 // To return char for a value. For example '2'
 // is returned for 2. 'A' is returned for 10. 'B'
 // for 11
+long long val(char c)
+{
+    if (c >= '0' && c <= '9')
+        return (long long)c - '0';
+    else
+        return (long long)c - 'A' + 10;
+}
+
+// To return char for a value. For example '2'
+// is returned for 2. 'A' is returned for 10. 'B'
+// for 11
 char reVal(long long num)
 {
     if (num >= 0 && num <= 9)
@@ -17,16 +28,35 @@ char reVal(long long num)
  
 // Function to convert a given decimal number
 // to a base 'base' and
-void deciToBase(long long n, long long base)
+void baseKtoBaseT(string n, long long baseK, long long baseT)
 {
+    long long len = n.size();
+    long long power = 1; // Initialize power of base
+    long long num = 0;  // Initialize result
+    long long i; 
     string res;
+ 
+    // Decimal equivalent is str[len-1]*1 +
+    // str[len-2]*base + str[len-3]*(base^2) + ...
+    for (i = len - 1; i >= 0; i--)
+    {
+        // A digit in input number must be
+        // less than number's base
+        if (val(n[i]) >= baseK){
+            printf("Invalid Number\n");
+            break;
+        }
+ 
+        num += val(n[i]) * power;
+        power = power * baseK;
+    }
  
     // Convert input number is given base by repeatedly
     // dividing it by base and taking remainder
-    while (n > 0)
+    while (num > 0)
     {
-        res.insert(0,1,reVal(n % base));
-        n /= base;
+        res.insert(0,1,reVal(num % baseT));
+        num /= baseT;
     }
     cout << res;
 }
