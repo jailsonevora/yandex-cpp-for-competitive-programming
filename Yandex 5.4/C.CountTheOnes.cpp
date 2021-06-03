720ac12e0bc48f75f3abee185410e4b7ca9a4ded
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-03 14:03:17 -0100

    C. Count The Ones

---
+++ Yandex 5.4/C.CountTheOnes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio();
+    cin.tie(0);
+    cout.tie(0);
+
+    long long n;
+    cin >> n;
+    
+    unsigned int count = 0;
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+    }
+    cout << count;
+}
