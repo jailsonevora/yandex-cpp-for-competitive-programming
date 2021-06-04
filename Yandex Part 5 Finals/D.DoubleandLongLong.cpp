c6913ed3739a4d22e49f8da6d9918522b166c270
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-04 12:45:06 -0100

    D. Double and Long Long

--- Yandex Part 5 Finals/D.DoubleandLongLong.cpp
+++ Yandex Part 5 Finals/D.DoubleandLongLong.cpp
@@ -3,26 +3,20 @@
 
 using namespace std;
 
+void f(double d){
+	unsigned long long i;
+    for (i = 1 << 64; i > 0; i = i / 2)
+        ((long long) d & i) ? printf("1") : printf("0");
+}
+
 int main(){
     
     ios::sync_with_stdio();
     cin.tie(0);
     cout.tie(0);
 
-    long long n;
-    cin >> n;
-    
-    unsigned int max = 0;
-    while (n) {
-        if( (n+max) == (n^max) )
-            max = n; 
-        n >>= 1;
-    }
-    cout << max;
-}
+    double d;
+    cin >> d;
 
-void f(double d){
-	unsigned long long i;
-    for (i = 1 << 64; i > 0; i = i / 2)
-        ((long long) d & i) ? printf("1") : printf("0");
+    f(d);
 }
