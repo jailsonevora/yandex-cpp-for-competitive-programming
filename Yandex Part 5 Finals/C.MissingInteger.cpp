86037e230e3602a406baf914a77bcb9c09efe7de
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-04 12:55:25 -0100

    A. Bit Representation of Double

---
+++ Yandex Part 5 Finals/C.MissingInteger.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio();
+    cin.tie(0);
+    cout.tie(0);
+
+    long n;
+    cin >> n;
+
+    vector<int> va = {2,4,6,8,10,12,14,16,18,20};
+    vector<int> vb = {}
+}
